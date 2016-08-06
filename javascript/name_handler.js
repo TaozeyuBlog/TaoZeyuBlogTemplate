9e80d273b7ccd0de00490be6afeee0fb121ae2e5
Moskize91 <xiangtantaozeyu@gmail.com>
2016-08-06 09:40:58 +0800

    change M to exports

--- javascript/name_handler.js
+++ javascript/name_handler.js
@@ -1,4 +1,4 @@
-M.date_from_article_name = function(title) {
+exports.date_from_article_name = function(title) {
     var matches = title.match(/^\d{4}-\d{2}-\d{2}/);
     if (matches) {
         return matches[0];
@@ -6,6 +6,6 @@ M.date_from_article_name = function(title) {
     return "";
 }
 
-M.title_from_article_name = function(title) {
+exports.title_from_article_name = function(title) {
     return title.replace(/\d{4}-\d{2}-\d{2}\s*/g, "");
 }
--- javascript/router.js
+++ javascript/router.js
@@ -1,5 +1,5 @@
 var Utils = require("/system/utils");
 
-M.path_of_article = function (article) {
+exports.path_of_article = function (article) {
     return /articles/ + Utils.convert_to_pinyin(article.name) + ".html";
 }
