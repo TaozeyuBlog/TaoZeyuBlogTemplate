19abdcbdb99ed80ccc9aa19559f9ea497a5e9b92
Moskize91 <xiangtantaozeyu@gmail.com>
2016-08-06 17:28:13 +0800

    allow groups

--- javascript/router.js
+++ javascript/router.js
@@ -4,6 +4,13 @@ exports.path_of_article = function (article) {
     return /article/ + Utils.convert_to_pinyin(article.name) + ".html";
 }
 
+exports.path_of_group = function(group, page_index) {
+    if (page_index == 0) {
+        return "/group/"+ group.name;
+    }
+    return "/group/"+ group.name +"/page/"+ page_index;
+}
+
 exports.path_of_total_article_page = function(page_index) {
     if (page_index == 0) {
         return "/";
